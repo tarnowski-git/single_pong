047182c5bb00d2c69d8e50bd2071b2875fb84edf
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-04 20:48:29 +0100

    Add drawBall()

--- game.js
+++ game.js
@@ -1,7 +1,20 @@
 let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2x");
+let ctx = canvas.getContext("2d");
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function draw() {
+    drawBall();
     requestAnimationFrame(draw);
 }
 requestAnimationFrame(draw);
