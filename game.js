068ecf8eeb7ddaa2c214b2a69c2d4f58d2893b13
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-04 21:48:55 +0100

    Add a paddle

--- game.js
+++ game.js
@@ -7,6 +7,10 @@ let dx = 2;
 let dy = -2;
 let ballRadius = 10;
 
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -15,9 +19,18 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clear all canvas size
     drawBall();
+    drawPaddle();
     // the x-coordinate (left-right)
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
